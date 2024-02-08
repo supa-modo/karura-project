19eac03e592b0fb8809ad185b2ca090ef76781f2
supa-modo <eddieochieng11@gmail.com>
2024-02-08 14:50:02 +0300

    Started new karura and friends demo website.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import './App.css';
+import React from "react";
+import "./app.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-         
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <h2>App</h2>;
+};
 
 export default App;
